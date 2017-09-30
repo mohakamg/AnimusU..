8a5630862c692d2c85da8fec4b8ddbd433f95d18
mohakamg <mohak.kant1amg@gmail.com>
2017-09-30 17:08:40 -0500

    Updated Mailer Setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { host: 'https://animusu.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'animusu.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
